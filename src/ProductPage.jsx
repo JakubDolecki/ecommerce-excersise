ff045c6e927692fa5a5f0212483baecab7ca1095
JakubDolecki <jakubdolecki1@wp.pl>
2023-12-13 03:47:03 +0100

    gallery in progress

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -588,7 +588,7 @@ export function ProductPage() {
         <GalleryContainer>
           <Gallery>
             <PicturesContainerGallery>
-              <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" className="closesvg">
+              <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg" className="closesvg">
                 <path
                   className="close"
                   d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
